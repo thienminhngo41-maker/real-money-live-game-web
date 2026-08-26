f2428645ddf0f15d2a695a2d21e6aa0fd1f29c02
thienminhngo41-maker <thienminhngo41@gmail.com>
2026-08-26 16:29:23 +0900

    refactor: move game data into data module

---
+++ data/games.js
@@ -0,0 +1,7 @@
+export const games=[
+ {id:'game1',image:'game1.png',url:'https://flappybird.io/',enabled:true},
+ {id:'game2',image:'game2.jpeg',url:'https://gridland.doublespeakgames.com/',enabled:true},
+ {id:'game3',image:'1.png',enabled:false},{id:'game4',image:'2.png',enabled:false},{id:'game5',image:'3.png',enabled:false}
+];
+export const providers=[{name:'PG전자',image:'4.png'},{name:'PRAGMATIC PLAY',image:'5.png'}];
+export const featuredGame={titleKey:'feature',subKey:'featureSub',image:'6.png'};
